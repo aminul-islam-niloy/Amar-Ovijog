6b6f998962e93bedabb3a980b30ade05a24a143b
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-10-14 12:19:10 +0600

    Database first Model Generation

--- AmarOvijog/Models/BdGeoServiceContext.cs
+++ AmarOvijog/Models/BdGeoServiceContext.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-
 namespace AmarOvijog.Models
 {
-    public partial class BdGeoServiceContext : IdentityDbContext<ApplicationUser>
+    public partial class BdGeoServiceContext : DbContext
     {
         public BdGeoServiceContext()
         {
@@ -22,12 +20,99 @@ namespace AmarOvijog.Models
         public virtual DbSet<Division> Divisions { get; set; } = null!;
         public virtual DbSet<Union> Unions { get; set; } = null!;
         public virtual DbSet<Upazila> Upazilas { get; set; } = null!;
-        public virtual DbSet<ApplicationUser> ApplicationUser { get; set; } = null!;
 
+   
 
-        public virtual DbSet<Complaint> Complaints { get; set; } = null!;
-        public virtual DbSet<ComplaintImage> ComplaintImages { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<District>(entity =>
+            {
+                entity.Property(e => e.BnName).HasMaxLength(25);
 
+                entity.Property(e => e.Lat).HasMaxLength(15);
 
+                entity.Property(e => e.Lon).HasMaxLength(15);
+
+                entity.Property(e => e.Name).HasMaxLength(25);
+
+                entity.Property(e => e.Url).HasMaxLength(50);
+
+                entity.HasOne(d => d.Division)
+                    .WithMany(p => p.Districts)
+                    .HasForeignKey(d => d.DivisionId)
+                    .HasConstraintName("FK_Districts_Divisions");
+            });
+
+            modelBuilder.Entity<Division>(entity =>
+            {
+                entity.Property(e => e.BnName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Bn_Name");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Url)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Union>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.BnName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Bn_name");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpazillaId).HasColumnName("Upazilla_id");
+
+                entity.Property(e => e.Url)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Upazilla)
+                    .WithMany(p => p.Unions)
+                    .HasForeignKey(d => d.UpazillaId)
+                    .HasConstraintName("FK__Unions__Upazilla__3F466844");
+            });
+
+            modelBuilder.Entity<Upazila>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.BnName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("Bn_name");
+
+                entity.Property(e => e.DistrictId).HasColumnName("District_id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Url)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("url");
+
+                entity.HasOne(d => d.District)
+                    .WithMany(p => p.Upazilas)
+                    .HasForeignKey(d => d.DistrictId)
+                    .HasConstraintName("FK__Upazilas__Distri__3C69FB99");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
--- AmarOvijog/Program.cs
+++ AmarOvijog/Program.cs
@@ -10,15 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add DbContext and Identity services
-builder.Services.AddDbContext<BdGeoServiceContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<BdGeoServiceContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<BdGeoServiceContext>();
+//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<BdGeoServiceContext>();
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<BdGeoServiceContext>()
-    .AddDefaultTokenProviders();
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//    .AddEntityFrameworkStores<BdGeoServiceContext>()
+//    .AddDefaultTokenProviders();
 
 // Learn more about configuring Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
